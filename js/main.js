986a26505cb5ec76c66af8ea5050bc52b90a37e3
Igor Natan <igornatan4@gmail.com>
2022-12-19 20:31:57 -0300

    commit função adiciona partes ao robotrom

--- js/main.js
+++ js/main.js
@@ -1,44 +1,45 @@
 const braco = document.querySelector("#braco");
 const controle = document.querySelectorAll("[data-controle]");
-const estatistica = document.querySelectorAll("[data-estatistica]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
 
 const pecas = {
   "bracos": {
-      "forca": 29,
-      "poder": 35,
-      "energia": -21,
-      "velocidade": -5
+    "forca": 29,
+    "poder": 35,
+    "energia": -21,
+    "velocidade": -5
   },
 
   "blindagem": {
-      "forca": 41,
-      "poder": 20,
-      "energia": 0,
-      "velocidade": -20
+    "forca": 41,
+    "poder": 20,
+    "energia": 0,
+    "velocidade": -20
   },
-  "nucleos":{
-      "forca": 0,
-      "poder": 7,
-      "energia": 48,
-      "velocidade": -24
+  "nucleos": {
+    "forca": 0,
+    "poder": 7,
+    "energia": 48,
+    "velocidade": -24
   },
-  "pernas":{
-      "forca": 27,
-      "poder": 21,
-      "energia": -32,
-      "velocidade": 42
+  "pernas": {
+    "forca": 27,
+    "poder": 21,
+    "energia": -32,
+    "velocidade": 42
   },
-  "foguetes":{
-      "forca": 0,
-      "poder": 28,
-      "energia": 0,
-      "velocidade": -2
+  "foguetes": {
+    "forca": 0,
+    "poder": 28,
+    "energia": 0,
+    "velocidade": -2
   }
 }
 
 controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
     manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    atualizaEstatisticas(evento.target.dataset.pecas)
   })
 })
 
@@ -50,4 +51,10 @@ function manipulaDados(operecao, controle) {
   } else {
     peca.value = parseInt(peca.value) + 1
   }
+}
+
+function atualizaEstatisticas(peca) {
+  estatisticas.forEach((elemento) => {
+    elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+  })
 }
